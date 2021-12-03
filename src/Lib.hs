fc8d9675485416c34f88db2a6ad32c3b22d7a9f3
kevinrsx <kevinlgc@foxmail.com>
2021-12-02 23:19:08 -0500

    Add Piece

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,17 @@
 module Lib
     ( someFunc
+    , Piece (..)
     ) where
+
+import Data.Vector
 
 someFunc :: IO ()
 someFunc = putStrLn "someFunc"
+
+type Board = Vector (Vector (Maybe Piece))
+
+data Piece = White | Black
+
+instance Show Piece where
+  show White = "o"
+  show Black = "x"
