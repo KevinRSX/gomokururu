3bca16dcd91470c7cdffc17460ff685c5a4d9c62
kevinrsx <kevinlgc@foxmail.com>
2021-12-18 16:34:04 -0500

    Add minimax

--- src/AI.hs
+++ src/AI.hs
@@ -8,13 +8,22 @@ where
 
 import Game
 import Data.Tree
+import Data.Vector as V
+  ( (!),
+    (//),
+  )
+
+import Data.Set as S
+  ( fromList,
+    toList
+  )
 
 getNextPos :: Board -> Piece -> Int -> (Int, Int)
 getNextPos _ Black step = (0, step `div` 2)
 getNextPos _ White step = (9, 9 - (step `div` 2 - 1))
 
-computeScore :: Board -> Int -> Int
-computeScore _ x = x
+computeScore :: Board -> Piece -> Int
+computeScore _ _ = 1000000
 
 -- Ref: 2019 project
 buildTree :: Piece -> Board -> [(Int, Int)] -> Int -> Tree Board
@@ -25,6 +34,47 @@ buildTree piece board neighbors lvl = Node board $ children lvl neighbors
           buildTree (reversePiece piece) (placePiece board piece row col) newNeighbors (lvl - 1) : children lvl xs
         newNeighbors = [(14, 14)]
 
+maxAlpha :: Piece -> Int -> Int -> Int -> Tree Board -> Int
+maxAlpha _ _ alpha _ (Node _ []) = alpha
+maxAlpha piece lvl alpha beta (Node b (x:xs))
+  | lvl == 0 = curScore
+  | canFinish curScore = curScore
+  | newAlpha >= beta = beta
+  | otherwise = maxAlpha piece lvl newAlpha beta (Node b xs)
+  where
+    curScore = computeScore b piece
+    canFinish score = score > 100000 || score < (-100000)
+    newAlpha = max alpha $ minBeta piece (lvl - 1) alpha beta x
+
+minBeta :: Piece -> Int -> Int -> Int -> Tree Board -> Int
+minBeta _ _ _ beta (Node _ []) = beta
+minBeta piece lvl alpha beta (Node b (x:xs))
+  | lvl == 0 = curScore
+  | canFinish curScore = curScore
+  | alpha >= newBeta = alpha
+  | otherwise = minBeta piece lvl alpha newBeta (Node b xs)
+  where
+    curScore = computeScore b piece
+    canFinish score = score > 100000 || score < (-100000)
+    newBeta = min beta $ maxAlpha piece (lvl - 1) alpha beta x
+
 -- Get a list (or vector) of points created by the next move
 expandBoard :: Board -> [(Int, Int)]
-expandBoard _  = [(7,8), (2,2)]
+expandBoard db = S.toList $ S.fromList $ ebHelper 0 0 db
+  where
+    ebHelper r c db
+      | c >= bdim = ebHelper (r + 1) 0 db
+      | r >= bdim || c >= bdim = []
+      | b ! r ! c == Empty = next
+      | otherwise = validNbPositions ++ next
+      where
+        b = getBoard db 
+        bdim = dim db
+        next = ebHelper r (c + 1) db
+        validNbPositions = [
+          (r + pr, c + pc) |
+            pr <- [-1 .. 1],
+            pc <- [-1 .. 1],
+            (pr, pc) /= (0, 0),
+            pieceValid db (r+pr) (c+pc)
+          ]
